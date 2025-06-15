56f6ce5b609d4f76ca181b23ae241dc04b9a39cd
Humayun Raza <humayunraza5@gmail.com>
2025-06-15 23:35:40 +0500

    changed server url ~H

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = 'http://localhost:5000/api'
+const BASE_URL = 'https://apiwaffleguy.azurewebsites.net/api'
 
 export default axios.create({
     baseURL:BASE_URL,
